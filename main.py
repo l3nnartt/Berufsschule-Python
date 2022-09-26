8a18caab4b5d34f85718238420b38d3bffec97cb
l.loesche@mm-bremen.de <loesche.lennart@gmail.com>
2022-09-26 18:51:06 +0200

    init main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Learn basic functions
+
+#print(-2 ** -3)
+#print(min('Abend', 'Aal', 'Ball'))
+
+#print(not(2.0>2))
+#
+#count = 10
+#count = count + 5
+#
+#x = 1
+#y = 2
+#product = x * y
+#print(product)
+
+
+#print((len('123')))
+
+#print((1<2) + 1)
+
+
+# Solve table
+#x=y=1
+##print(x)
+##print(y)
+#x=2
+##print(x)
+##print(y)
+#z=x
+##print(x)
+##print(y)
+##print(z)
+#z = z* 3
+##print(x)
+##print(y)
+##print(z)
+#x,y,z = x,x,x
+##print(x)
+##print(y)
+##print(z)
+#x = 'y'
+#print(x)
+#print(y)
+#print(z)
+#y = 2>z
+#print(x)
+#print(y)
+#print(z)
+#y = min(2,z,5)
+#print(x)
+#print(y)
+#print(z)
