fdf1d929d93817f51aba156b54f35f43a91725ee
l.loesche@mm-bremen.de <loesche.lennart@gmail.com>
2022-10-10 11:06:19 +0200

    clean up 04

--- 04_Einstiegsaufgaben.py
+++ 04_Einstiegsaufgaben.py
@@ -1,19 +1,6 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Starting 04_Einstiegsaufgaben.py')
 
 # Aufgabe 0:
 # Schreibe ein Programm, welches in der Konsole die Worte "Hello World" ausgibt.
@@ -26,8 +13,7 @@ print("Hello World")
 # subtrahiert, multipliziert und dividiert und die Ergebnisse in der Konsole ausgibt.
 # Wähle sowohl für die zwei Zahlen als auch die Ergebnisse der vier Grundrechenarten geeignete Variablen.
 
-def Grundrechenarten(a, b):  # functions should be lowercase x.x
-
+def Grundrechenarten(a, b):
     # first calculate the results
     add = a + b
     sub = a - b
