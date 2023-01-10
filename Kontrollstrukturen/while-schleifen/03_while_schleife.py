53416b9c3653ff03d2f1ef72659e44a8b0cd08ec
l.loesche@mm-bremen.de <loesche.lennart@gmail.com>
2023-01-10 15:09:36 +0100

    create folder + add while tasks

---
+++ Kontrollstrukturen/while-schleifen/03_while_schleife.py
@@ -0,0 +1,28 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Starting while_schleife.py')
+
+print("### Aufgabe 1 ###")
+# Schreibe ein Programm, welches die Werte von 0 bis 5 ausgibt.
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+
+
+print("### Aufgabe 2 ###")
+# Schreibe ein Programm, welches die Werte von 2 bis 7 ausgibt.
+i = 2
+while i < 8:
+    print(i)
+    i += 1
+
+
+print("### Aufgabe 3 ###")
+# Schreibe ein Programm, welches die Werte von 1 bis 19 in Zweierschritten ausgibt.
+i = 1
+while i < 20:
+    print(i)
+    i += 2
+
+print("### Aufgabe 4 ###")
