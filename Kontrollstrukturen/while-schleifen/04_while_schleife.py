b3bf8ba88c520772e6f3e9c40eefd73712befb9f
l.loesche@mm-bremen.de <loesche.lennart@gmail.com>
2023-02-21 14:41:47 +0100

    credit calc

--- Kontrollstrukturen/while-schleifen/04_while_schleife.py
+++ Kontrollstrukturen/while-schleifen/04_while_schleife.py
@@ -39,7 +39,47 @@ print("### Aufgabe 2 ###")
 # Schreibe ein Programm welches die Summe der Zahlen von 1 bis 100 berechnet.
 summe = 1
 multiplier = 1
-while multiplier < 100:
+while multiplier < 101:
     multiplier += 1
     summe += multiplier
 print("Die Summe der Zahlen von 1 bis 100 ist: ", summe)
+
+print("### Aufgabe 3 ###")
+# Schreibe ein Programm welches die Rückzahlung eines Kredites berechnet. Folgende Eingaben werden vom Benutzer
+# vorgenommen:
+#
+# die Höhe des Kredites
+# der Zinssatz in Prozent
+# der jährlicher Rückzahlungsbetrag
+#
+# Die Ausgabe des Programms soll nach folgender Art erfolgen (Werte der Variablen können frei gewählt werden):
+#
+#
+# Kreditsumme in Euro: 1000
+#     Zinssatz    in    Prozent:    6
+#     Jährlicher    Rückzahlungsbetrag    in    Euro:    200
+#     2004    Zinsen:    60    Euro    Tilgung:    140    Euro     Rest:    860    Euro
+#     2005    Zinsen:    51    Euro    Tilgung:    149    Euro     Rest:    711    Euro
+#     2006    Zinsen:    42    Euro    Tilgung:    158    Euro     Rest:    553    Euro
+#     2007    Zinsen:    33    Euro    Tilgung:    167    Euro     Rest:    386    Euro
+#     2008    Zinsen:    23    Euro    Tilgung:    177    Euro     Rest:    209    Euro
+#     2009    Zinsen:    12    Euro    Tilgung:    188    Euro     Rest:    21     Euro
+#     Restforderung:     21    Euro
+# Kreditsumme
+credit = float(input("Bitte geben Sie die Kreditsumme in Euro ein: "))
+
+# Zinssatz
+interest = float(input("Bitte geben Sie den Zinssatz in Prozent ein: "))
+
+# Rückzahlungsbetrag
+repayment = float(input("Bitte geben Sie den Rückzahlungsbetrag in Euro ein: "))
+
+# Ausgabe
+year = 2004
+while credit > 0:
+    interest = credit * (interest / 100)
+    credit -= repayment
+    print(year, "Zinsen: ", interest, "Euro", "Tilgung: ", repayment, "Euro", "Rest: ", credit, "Euro")
+    year += 1
+
+print("Restbetrag: ", credit, "Euro")
