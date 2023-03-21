fda527262adc0376df59c8840642f7222e50ce07
l.loesche <l.loesche@mm-bremen.de>
2023-03-21 11:07:23 +0100

    reverse string

---
+++ Kontrollstrukturen/reversestring.py
@@ -0,0 +1,17 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Starting 04_Palindrom.py')
+
+print("### Aufgabe 1 ###")
+
+
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
+# Schreiben Sie das Pythonprogramm reverse_string.py,
+# welches einen String von der Konsole einliest und ihn umgekehrt wieder ausgibt.
+
+def reverseString(inputString):
+    return inputString[::-1]
+
+
+string = input("Bitte geben Sie einen String ein: ").lower()
+print("Der umgekehrte String ist: ", reverseString(string))
