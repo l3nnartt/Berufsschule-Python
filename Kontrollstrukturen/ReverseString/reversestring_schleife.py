64846bd94e807a668f2a79604d95f01d910526b4
l.loesche <l.loesche@mm-bremen.de>
2023-03-21 14:08:08 +0100

    add other methods to reverse a string

---
+++ Kontrollstrukturen/ReverseString/reversestring_schleife.py
@@ -0,0 +1,26 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Starting 04_Palindrom.py')
+
+print("### Aufgabe 1 ###")
+
+
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
+# Schreiben Sie das Pythonprogramm reverse_string.py,
+# welches einen String von der Konsole einliest und ihn umgekehrt wieder ausgibt.
+
+string = "Hallo Welt!"
+reversedString = ""
+reversedString2 = ""
+
+# Option 1
+for x in range(len(string), -1, -1):
+    reversedString += string[x]
+    print(reversedString)
+    # !tleW ollaH
+
+# Option 2
+for c in string:
+    reversedString2 = c + reversedString2
+    print(reversedString2)
+    # !tleW ollaH
