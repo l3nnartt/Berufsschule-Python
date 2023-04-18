33e4a9653c0a65385b5505fa3d6fa57ff5f75308
l.loesche <l.loesche@mm-bremen.de>
2023-04-18 14:49:56 +0200

    adjust some things

---
+++ Komplexe-Kontrollstrukturen/02_Zweidimensionale_Listen.py
@@ -0,0 +1,24 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Starting 02_Zweidimensionale_Listen.py')
+
+# Arbeitsauftrag
+# Schreibe ein Programm, welches eine zweidimensionale Liste von Wörtern durchläuft und die Anzahl der Buchstaben zählt,
+# die in den Wörtern enthalten sind.
+#
+# Folgende Liste ist gegeben:
+# [['Hund', 'Katze', 'Maus'], ['Apfel', 'Banane', 'Erdbeere'], ['Gurke', 'Tomate', 'Kartoffel']]
+#
+# Das Ergebnis wäre wie folgt, wenn nach einem “a” bzw. “A” gesucht wird:
+# “Der Buchstabe A kommt 7 mal in der vorgegeben Liste vor”
+#
+liste = [['Hund', 'Katze', 'Maus'], ['Apfel', 'Banane', 'Erdbeere'], ['Gurke', 'Tomate', 'Kartoffel']]
+counter = 0
+
+for i in liste:
+    for j in i:
+        for k in j:
+            if k.upper() == "A":
+                counter += 1
+
+print("Der Buchstabe A kommt " + str(counter) + " mal in der vorgegeben Liste vor")
--- Kontrollstrukturen/04_ String_howOftenContains.py
+++ Kontrollstrukturen/04_ String_howOftenContains.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
 print("### Aufgabe 1 ###")
 # Fur Strings ist eine Funktion: howOftenContaind(char* str, char c) zu implementieren.
 # Es soll berechnet werden, wie oft das Zeichen c im String str enthalten ist.
-def howOftenContaind(str, c):
+
+
+def howOftenContains(string, c):
     counter = 0
-    for character in str:
+    for character in string:
         if character == c:
             counter += 1
     return counter
@@ -17,4 +19,4 @@ def howOftenContaind(str, c):
 
 text = input("Bitte geben Sie einen String ein: ")
 char = input("Bitte geben Sie ein zu suchendes Zeichen ein: ")
-print("Die Anzahl der Vorkommen von " + char + " im String " + text + " ist: ", howOftenContaind(text, char))
+print("Die Anzahl der Vorkommen von " + char + " im String " + text + " ist: ", howOftenContains(text, char))
