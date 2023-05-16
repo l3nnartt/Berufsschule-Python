068900ae9b69eb760b38ff59c51db1968087b21d
Lennart Lösche <l.loesche@mm-bremen.de>
2023-05-16 14:05:21 +0200

    add tupel task

---
+++ Variablen-Datentypen-Operatoren/06_Tupel.py
@@ -0,0 +1,24 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+  print('Starting 06_Tupel.py')
+
+# Schreibe ein Skript, das das Mischen der 32 Karten eines Skatspiels simuliert.
+# Jede Karte wird durch ein Tupel der Form (Farbe, Wert) dargestellt.
+# Farben sind: Kreuz, Pik, Herz und Karo.
+# Werte sind: Ass, König, Dame, Bube, Zehn, Neun, Acht, Sieben.
+#
+# Ein Beispiel für ein Karten-Tupel wäre also:
+# ("Kreuz", "Ass")
+# oder
+# ("Pik", "Zehn").
+# Wie Tupel erstellt werden, kannst du in "02 Tupel" noch einmal nachlesen.
+
+# Aufgabe 1: Erstellen das Skatdecks.
+# Zunächst soll eine Liste mit den 32 Karten-Tupeln erstellt werden. Die
+# Karten-Tupel befinden sich also als Einträge in einer Liste. Dazu müssen
+# die einzelnen Tupel erstellt werden und anschließend der Liste hinzugefügt
+# werden. Dies kann vorteilhaft über geschachtelte for-Schleifen geschehen.
+# Gib die erste und die letzte Karte des Kartenstapels in der Konsole aus.
+# Gib das ungemischte komplette Skatdeck anschließend in der Konsole aus.
+# (Schleife benutzen)
+
